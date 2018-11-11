a8aba6b3843ef25818b5eb12d3e834c408096799
FalconWarriorr <niall.p.mullane@gmail.com>
2018-11-11 13:03:59 -0500

    super basic thermal model in heating zone woking on building model

--- building_model.py
+++ building_model.py
@@ -1,1 +1,43 @@
+import matplotlib.pyplot as plt
+import heating_zone as hz
 #This file instantiates all the rooms and updates their temperatures
+morning = 7.0
+day = 12.0
+night = 20.0
+
+
+class building:
+    def __init__(self, num_zones, zones=[]):
+        self.num_zones = num_zones
+        self.zones = zones
+        self.time = []
+    def update_zones(self, target_temps, outside_temp=-1, timestep_min=5.0, heating_constants = [0.020]):
+        for i in range(self.num_zones):
+            if len(heating_constants == 1):
+                self.zones[i].update_temp(target_temps[i], outside_temp, timestep_min, heating_constants)
+            else:
+                self.zones[i].update_temp(target_temps[i], outside_temp, timestep_min, heating_constants[i])
+        self.time.append(self.time[-1] + timestep_min / 60)
+    def plot_zone_data_over_time(self, zones, var):
+        plt.figure(1)
+        plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y']) + cycler('linestyle', ['-', '--', ':', '-.'])))
+        for i in range(len(zones)):
+            plt.plot(zones[i].time, getattr(zones[i], var))
+        plt.show()
+
+
+if __name__=="__main__":
+    zone1 = hz.heating_zone(25, 20, 18, 1.5)
+    zone2 = hz.heating_zone(28, 17, 20, 1.5)
+    zone3 = hz.heating_zone(31, 23, 25, 1.5)
+
+    while zone.time[-1] < 48:
+        if(zone.time[-1]%24.0 >= morning and zone.time[-1]%24.0 < day):
+            zone.update_temp(zone.morning_temp)
+        elif(zone.time[-1]%24.0 >= day and zone.time[-1]%24.0 < night):
+            zone.update_temp(zone.day_temp)
+        else:   
+            zone.update_temp(zone.night_temp)
+    house = building(
+
+
--- heating_zone.py
+++ heating_zone.py
@@ -1,8 +1,7 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import building_model as bld
+
 #This file creates and tracks rooms and determines temperature within a room based on surrounding rooms and oustide temperature
-morning = 7
-day = 12
-night = 20
 
 class heating_zone:
     def __init__(self, morning_temp, day_temp, night_temp, wattage, size=1):
@@ -16,18 +15,26 @@ class heating_zone:
         self.temp.append(self.morning_temp)
         self.wattage = wattage
         self.energy_usage.append(0)   #KwH
-        self.time.append(morning)
+        self.time.append(bld.morning)
         
-    def update_temp(self, target_temp, outside_temp=-1, timestep_min=5, heating_constant = .01):
-        self.temp.append(timestep_min * heating_constant * (target_temp - self.temp[-1]))
+    def update_temp(self, target_temp, outside_temp=-1, timestep_min=5.0, heating_constant = .020):
+        self.temp.append( self.temp[-1] + timestep_min * heating_constant * (target_temp - self.temp[-1]))
         self.energy_usage.append(self.energy_usage[-1] + self.wattage  * timestep_min / 60)
         self.time.append(self.time[-1] + timestep_min / 60)
-    def graph_temp(self):
-        plot(self.temp, self.time)
+        #print self.temp[-1]
+    def graph_over_time(self, var):
+        plt.plot(self.time, var)
+        plt.show()
 
 
 if( __name__ == "__main__"):
-    zone = heating_zone(25, 20, 18, 1500)
-    while zone.time[-1] < 24:
-        zone.update_temp(30)
-    zone.graph_temp()
+    zone = heating_zone(25, 20, 18, 1.5)
+    while zone.time[-1] < 48:
+        if(zone.time[-1]%24.0 >= bld.morning and zone.time[-1]%24.0 < bld.day):
+            zone.update_temp(zone.bld.morning_temp)
+        elif(zone.time[-1]%24.0 >= bld.day and zone.time[-1]%24.0 < bld.night):
+            zone.update_temp(zone.day_temp)
+        else:
+            zone.update_temp(zone.night_temp)
+    zone.graph_over_time(zone.temp)
+    #zone.graph_over_time(zone.energy_usage)
