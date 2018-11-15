5180f5189df39640043743bc99f5490529d54ff3
FalconWarriorr <niall.p.mullane@gmail.com>
2018-11-15 00:21:43 -0500

    improved graphing created useful scripts to work with Rplots

--- activity_data_analyzer.R
+++ activity_data_analyzer.R
@@ -13,19 +13,30 @@ activities <- read.table("atus_00001.dat", header=T, sep = " ")
 
 #Take activities at home
 not_at_home <- activities[activities$Location != 101,]
+start_times <- not_at_home$Start_time
+summary(start_times)
+substringed_data <- sapply(start_times, substring, 1, 2)
+substringed_data
+
 
 #Analyze structure
 summary(not_at_home)
 dim(not_at_home)
 
+
+#GUI Things
 x11()
 
 
 #par(mfrow=c(1,2))
+
+#Graph histograms of activity duration
 dev.new()
 hist(activities$Duration)
 dev.new()
 hist(not_at_home$Duration)
 
+dev.new()
+hist(not_at_home$Start_time, freq = FALSE, density=not_at_home$duration)
 
 locator(1)
