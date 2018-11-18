fe1fb77ebb8b7ce4c553777a33ab6aa236c148de
Meekhel Patel <patel19m@ncssm.edu>
2018-11-18 00:43:33 -0500

    master file

---
+++ Model/master.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import heating_zone as hz
+import tensorflow as tf
+import numpy as np
+import schedule_generator as sg
+import classify as nw
+from cycler import cycler
