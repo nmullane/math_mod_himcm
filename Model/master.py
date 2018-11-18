f5e573f7b6feae84cb2040630b41b14df11f1822
Meekhel Patel <patel19m@ncssm.edu>
2018-11-18 00:50:43 -0500

    changed regression model name

--- Model/master.py
+++ Model/master.py
@@ -3,5 +3,6 @@ import heating_zone as hz
 import tensorflow as tf
 import numpy as np
 import schedule_generator as sg
-import classify as nw
+import classify as cf
+import regression as rg
 from cycler import cycler
