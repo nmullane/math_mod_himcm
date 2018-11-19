27ee96044550759290fbc33fb46a219026243c42
FalconWarriorr <niall.p.mullane@gmail.com>
2018-11-18 23:28:44 -0500

    schedule generator now greatly improved. has Fixed mode plus controllable variationt

--- DataAnalysis/clustering.R
+++ DataAnalysis/clustering.R
@@ -2,6 +2,8 @@ rm(list=ls())
 
 setwd("~/himcm/DataAnalysis")
 library(ggplot2)
+library(fpc)
+library(factoextra)
 source("myfunctions.R")
 
 activities <- read.table("not_at_home_activities.csv", header=T, sep=",")
@@ -24,14 +26,46 @@ summary(activities)
 #aggregate(mydata,by=list(fit$cluster),FUN=mean)
 ## append cluster assignment
 #mydata <- data.frame(mydata, fit$cluster)
-
-
 # K-Means Clustering with 5 clusters
-fit <- kmeans(activities[c(7, 6)])
+set.seed(320)
+df <- activities[,c(6,5)]
+summary(df)
+clusters <- kmeans(df, 13)
+db <- fpc::dbscan(df[50000:75000,], eps=2.31, MinPts=10)
 
-# vary parameters for most readable graph
-library(cluster) 
-clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
-    # Centroid Plot against 1st 2 discriminant functions
-library(fpc)
-plotcluster(activities[c(7,6)], fit$cluster)
+str(clusters)
+
+X11()
+clusters$cluster<- as.factor(clusters$cluster)
+ggplot(activities, aes(start_time, Duration, color = clusters$cluster)) + geom_point()
+X11()
+plot(db, df[50000:75000,], main = "DBSCAN", frame = FALSE)
+X11()
+fviz_cluster(db, df[50000:75000,], stand = FALSE, frame = FALSE, geom = "point", ellipse = FALSE)
+X11()
+dbscan::kNNdistplot(df, k =  5)
+abline(h = 1.15, lty = 2)
+
+
+print(db)
+summary(db)
+names(db)
+write.csv(db$cluster, "clusters.csv")
+write.csv(df, "data_frame.csv")
+
+#par(mfrow=c(1,90))
+#dev.new()
+##qplot(activities[50000:75000,6], activities[50000:75000,5], xlab="Start time (min)", ylab="Outing Duration")
+#ggplot(activities, aes(start_time, Duration)) + geom_point()
+#
+#
+## vary parameters for most readable graph
+#library(cluster) 
+#clusplot(activities[50000:75000,6:5], fit$cluster, color=TRUE, shade=TRUE, labels=0, lines=0)
+#    # Centroid Plot against 1st 2 discriminant functions
+#library(fpc)
+#plotcluster(activities[50000:75000,5:6], fit$cluster)
+#message("Press Return To Continue")
+#invisible(readLines("stdin", n=1))
+while (!is.null(dev.list())) Sys.sleep(1)
+
--- Model/schedule_generator.py
+++ Model/schedule_generator.py
@@ -4,12 +4,13 @@ import numpy as np
 # Test, Fixed, Progressing, 
 class Schedule:
 
-    def __init__(self, scheduleType="Fixed", wake_up=7.0, sleep=11.0, typical=1, num_outings=1, variation=0, abrupt_change_per_year=0, test_id=0):
+    def __init__(self, scheduleType="Test", wake_up=7.0, sleep=11.0, typical=1, num_outings=1, variation_weekday=0, variation_weekend=0, abrupt_change_per_year=0, test_id=0, progression_rate = 0):
         self.scheduleType = scheduleType
         self.wake_up = wake_up
         self.sleep = sleep
         self.typical = typical
-        self.variation = variation
+        self.variation_weekday = variation_weekday
+        self.variation_weekend = variation_weekend
         self.abrupt_change_per_year = abrupt_change_per_year
         self.functions = {}
         self.functions = {
@@ -18,25 +19,74 @@ class Schedule:
             'Progressing': self.getTimesProgressing,
         }
         
-        self.activities_data = data_parser.load_data(os.path.abspath(os.path.dirname(sys.argv[0])) + "/Data/not_at_home_activities.csv")
-        self.id = test_id
-        #print(self.activities_data[self.id][0])
+        self.activities_data = np.array(data_parser.load_data(os.path.abspath(os.path.dirname(sys.argv[0])) + "/Data/not_at_home_activities.csv"))
+        idx = np.r_[0:1, 50001:75002]
+        self.activities_data = self.activities_data[idx]
 
-        self.typical_outings = []  #form  [leave_time, return_time, ... ,retiurn_time]
-         
+        self.activities_data = np.append(self.activities_data, np.array(data_parser.load_data(os.path.abspath(os.path.dirname(sys.argv[0])) + "/Data/clusters.csv")), axis=1)
+        #print(self.activities_data[0])
+
+        self.clustered_data = []
+        for row in self.activities_data:
+            #print row
+            if row[7] != '0':
+                self.clustered_data.append(row)
+        #print self.clustered_data.shape
+
+            
+        #print(self.activities_data[self.test_id][0])
+
+        self.test_id = test_id
+        self.rand_id = np.random.randint(1, max(self.activities_data.shape))
+        self.num_outings = num_outings
+
+        self.base_start_times = []
+        self.base_durations = []
+
+        self.clust_id = np.random.randint(1, len(self.clustered_data))
+        self.past_clust_id = [0]
+        for i in range(num_outings):
+            #print self.clustered_data[self.clust_id][7]
+            #print self.past_clust_id
+            while self.clustered_data[self.clust_id][7] in self.past_clust_id:
+                self.clust_id = np.random.randint(1, len(self.clustered_data))
+            self.past_clust_id.append(self.clustered_data[self.clust_id][7])
+
+            self.base_start_times.append(float(self.clustered_data[self.clust_id][4]))
+            self.base_durations.append(float(self.clustered_data[self.clust_id][3]))
+        print self.base_start_times
+        print self.base_durations
+            
+        self.base_weekend_start_times = []
+        self.base_weekend_durations = []
+
+        self.clust_id = np.random.randint(1, len(self.clustered_data))
+        self.past_clust_id = [0]
+        for i in range(num_outings):
+            #print self.clustered_data[self.clust_id][7]
+            #print self.past_clust_id
+            while self.clustered_data[self.clust_id][7] in self.past_clust_id:
+                self.clust_id = np.random.randint(1, len(self.clustered_data))
+            self.past_clust_id.append(self.clustered_data[self.clust_id][7])
+
+            self.base_weekend_start_times.append(float(self.clustered_data[self.clust_id][4]))
+            self.base_weekend_durations.append(float(self.clustered_data[self.clust_id][3]))
+
+        print self.base_weekend_start_times
+        print self.base_weekend_durations
         
     #1440x7
     def getTimesTest(self, day_start, day_end):
-        start_times_mins = float(self.activities_data[self.id][4]) * 60.0
-        end_times_mins = float(self.activities_data[self.id][5]) * 60.0
+        start_times_mins = float(self.activities_data[self.test_id][4]) * 60.0
+        end_times_mins = float(self.activities_data[self.test_id][5]) * 60.0
 
-        start_times_mins_2 = float(self.activities_data[self.id+100][4]) * 60.0
-        end_times_mins_2 = float(self.activities_data[self.id+100][5]) * 60.0
-        #print self.activities_data[self.id][4]
-        #print self.activities_data[self.id][5]
+        start_times_mins_2 = float(self.activities_data[self.test_id+100][4]) * 60.0
+        end_times_mins_2 = float(self.activities_data[self.test_id+100][5]) * 60.0
+        #print self.activities_data[self.test_id][4]
+        #print self.activities_data[self.test_id][5]
 
-        #print self.activities_data[self.id+100][4]
-        #print self.activities_data[self.id+100][5]
+        #print self.activities_data[self.test_id+100][4]
+        #print self.activities_data[self.test_id+100][5]
 
         #print start_times_mins
         #print end_times_mins
@@ -63,15 +113,50 @@ class Schedule:
             
 
     def getTimesFixed(self, day_start, day_end):
+        num_days = day_end - day_start + 1
         times = []
-        num_days = day_end - day_start
+        start_times = []
+        durations = []
+        start_times_weekend = []
+        durations_weekend = []
+        
+        #Modulate start and duration for the day
+        for i in range(len(self.base_start_times)):
+            start_times.append(self.base_start_times[i] + np.random.ranf() * self.variation_weekday - self.variation_weekday/2.0)
+            durations.append(self.base_durations[i] + np.random.ranf() * self.variation_weekday * 60.0 - self.variation_weekday/2.0 * 60.0)
+        for i in range(len(self.base_weekend_start_times)):
+            start_times_weekend.append(self.base_weekend_start_times[i] + np.random.ranf() * self.variation_weekend - self.variation_weekend/2.0)
+            durations_weekend.append(self.base_weekend_durations[i] + np.random.ranf() * self.variation_weekend * 60.0 - self.variation_weekend/2.0 * 60.0)
+            
+
+        for i in range(1440):
+            times.append([])
+            for j in range(num_days):
+                times[i].append(1)
+        for j in range(num_days):
+            for i in range(1440):
+                if j < 5:
+                    for k in range(len(start_times)):
+                        print k
+                        if i >= start_times[k] * 60.0  and i < start_times[k] * 60.0 + durations[k]:
+                            #print k
+                            times[i][j] = 0
+                else:
+                    for k in range(len(start_times_weekend)):
+                        if i >= start_times_weekend[k] * 60.0 and i < start_times_weekend[k] * 60.0 + durations_weekend[k]:
+                            times[i][j] = 0
+        return np.array(times)
     def getTimesProgressing(self, day_start, day_end):
         times = []
-        num_days = day_end - day_start
+        num_days = day_end - day_start + 1
+
+
     def getTimes(self, day_start, day_end):
-        return functions[scheduleType](day_start, day_end)
+        return self.functions[self.scheduleType](day_start, day_end)
 
 if __name__=="__main__":
-    sched = Schedule(test_id=10024)
-    times = sched.getTimesTest(7, 7)
+    sched = Schedule(scheduleType = "Fixed", num_outings = 4, test_id=10024, variation_weekday = .1, variation_weekend = .7)
+    times = sched.getTimes(7, 7)
+    np.set_printoptions(threshold=np.inf)
+    #print times
     #print times.shape
